2e50b19fdae4178e01777ea8b738f66203d3ab2d
legna <legnajaneth@hotmail.com>
2024-05-11 00:31:33 -0700

    Assignment 5 fancifymytext html, css and js file updated

--- fancifymytext.js
+++ fancifymytext.js
@@ -4,5 +4,25 @@ alert("Hello World!");
 }
 
 function makeBigger() {
-    document.getElementById("textInput").style.fontSize = "24pt";
+    document.getElementById("textInput").style.fontSize = "4em";
+}
+
+function fancify(){
+    document.getElementById("textInput").style.color = "blue";
+    document.getElementById("textInput").style.fontWeight = "bold";
+    document.getElementById("textInput").style.textDecoration = "underline"
+}
+
+function boring(){
+    document.getElementById("textInput").style.color = "";
+    document.getElementById("textInput").style.fontWeight = "";
+    document.getElementById("textInput").style.textDecoration = "";
+    document.getElementById("textInput").style.fontSize = "";
+}
+
+function moo(){
+    var input = document.getElementById("textInput");
+    input.value = input.value.toUpperCase();
+    var part = input.value.split(".");
+    input.value = part.join("MOO");
 }
